4287fd4c79ed15a8170b6546a04f2ded005780bc
Felipe Endo <felipecs.endo@gmail.com>
2018-04-22 16:52:40 +0000

    Config, devise and fixes
    Member and Admin Devise models
    Category model
    Seeds
    stdout.sync in development.rb for foreman output

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,14 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  
+  #Foreman output
+  $stdout.sync = true
   # Better_errors
-  BetterErrors::Middleware.allow_ip! '189.121.198.111'
+  BetterErrors::Middleware.allow_ip! ENV['TRUSTED_IP'] if ENV['TRUSTED_IP']
+  
+  #Devise config
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
