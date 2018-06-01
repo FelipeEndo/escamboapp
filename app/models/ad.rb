8cdfeaa2eb183d8689eddd3df5273e5d2a4fdb53
Felipe Endo <felipecs.endo@gmail.com>
2018-06-01 02:47:24 +0000

    Ad Details

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -10,7 +10,7 @@ class Ad < ActiveRecord::Base
   #gem money-rails
   monetize :price_cents
   
-  has_attached_file :picture, styles: { medium: "320x150#", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :picture, styles: { large: '800x300#', medium: "320x150#", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
   
   # Scopes
